e903a1593a6c59b25c2f41dc76d26c4538a64204
CarlosIvanMunozVillazon <93873199+CarlosIvanMunozVillazon@users.noreply.github.com>
2023-09-10 11:24:23 -0500

    5/8 methods implemented LinkedListNoTail

--- C++LinkedListNoTail/src/classes/LinkedList.cpp
+++ C++LinkedListNoTail/src/classes/LinkedList.cpp
@@ -11,12 +11,12 @@ public:
     int data;
     Node *next;
 
-    Node ()
+    Node()
     {
         this->next = NULL;
     }
 
-    Node (int data = -1, Node *next = 0) // the datatype of the second parameter must be a pointer.
+    Node(int data = -1, Node *next = 0) // the datatype of the second parameter must be a pointer.
     {
         this->data = data,
         this->next = next;
@@ -34,9 +34,91 @@ public:
 class LinkedList
 {
 private:
-    // private stuff
+    // By making the head private, we ensure our data integrity.
+    Node *head;
+
 protected:
     // protected stuff
 public:
-    // public stuff
+    LinkedList()
+    {
+        this->head = NULL;
+    }
+
+    ~LinkedList()
+    {
+        Node *ref = head;
+
+        while (ref != NULL)
+        {
+            ref = ref->next; // move reference to the next node
+            delete head;     // delete the current head
+            head = ref;      // update the head for repeating the process
+        }
+
+        ref = NULL;
+        head = NULL;
+    }
+
+    // Methods to implement:
+    // 1. isEmpty *
+    // 2. pushBack *
+    // 3. pushFront *
+    // 4. popFront *
+    // 5. popBack 
+    // 6. deleteNode
+    // 7. belongsToList
+    // 8. printList *
+
+    bool isEmpty()
+    {
+        return head == NULL;
+    }
+
+    void printList()
+    {
+        Node *ref = head;
+        while (ref->next != NULL)
+        {
+            cout << ref->data << "--> " << endl;
+            ref = ref->next;
+        }
+        cout << ref->data << endl;
+        ref = NULL;
+    }
+
+    void pushFront(int data)
+    {
+        // just point the the head to a new allocated node, and the new allocated node point to the previous value of the head
+        head = new Node(data, head);
+    }
+
+    void pushBack(int data)
+    {
+        Node *ref = head;
+
+        while (ref->next != NULL)
+        {
+            ref = ref->next;
+        }
+
+        ref->next = new Node(data, NULL);
+        ref = NULL;
+    }
+
+    int popFront()
+    {
+        int deletedElement = -1;
+        if (head != NULL)
+        {
+            Node *ref = head; // point to the head
+            deletedElement = head->data; // extract the data before deleting it
+            ref = ref->next; // move the reference to the next node
+            delete head; // delete the content of the current head
+            head = ref; // move the head to the reference
+            ref = NULL; // make the reference node point to null for avoiding memory leaks
+        }
+
+        return deletedElement;
+    }
 };
