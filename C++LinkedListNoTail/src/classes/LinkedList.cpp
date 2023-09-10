2c1a123a33a51582256f12864bee56254adf123e
CarlosIvanMunozVillazon <93873199+CarlosIvanMunozVillazon@users.noreply.github.com>
2023-09-10 10:52:15 -0500

    node class implemented no tail list

--- C++LinkedListNoTail/src/classes/LinkedList.cpp
+++ C++LinkedListNoTail/src/classes/LinkedList.cpp
@@ -8,7 +8,27 @@ private:
 protected:
     // protected stuff
 public:
-    // public stuff
+    int data;
+    Node *next;
+
+    Node ()
+    {
+        this->next = NULL;
+    }
+
+    Node (int data = -1, Node *next = 0) // the datatype of the second parameter must be a pointer.
+    {
+        this->data = data,
+        this->next = next;
+    }
+
+    ~Node()
+    {
+        if (this->next != 0) // the pointers to the next nodes should be pointed to 0 for avoiding memory leaks.
+        {
+            this->next = 0;
+        }
+    }
 };
 
 class LinkedList
