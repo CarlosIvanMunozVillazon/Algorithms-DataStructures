507e74ed719a787ec847272a0564d985081ae713
CarlosIvanMunozVillazon <93873199+CarlosIvanMunozVillazon@users.noreply.github.com>
2023-09-10 10:35:48 -0500

    init setup linkedlist no tail

---
+++ C++LinkedListNoTail/src/classes/LinkedList.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+private:
+    // private stuff
+protected:
+    // protected stuff
+public:
+    // public stuff
+};
+
+class LinkedList
+{
+private:
+    // private stuff
+protected:
+    // protected stuff
+public:
+    // public stuff
+};
