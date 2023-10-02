dd318d07e01465855e51230d82056e8491e244d8
CarlosIvanMunozVillazon <93873199+CarlosIvanMunozVillazon@users.noreply.github.com>
2023-10-01 19:16:18 -0500

    Pointer referencing brush up

--- C++MergeSort/src/main.cpp
+++ C++MergeSort/src/main.cpp
@@ -1,31 +1,29 @@
 #include <iostream>
 using namespace std;
 
-void printNumber(int *printNumber);
-
-int main ()
+int main()
 {
+    int array[5] = {1, 2, 3, 4, 5};
 
-    int variable1 = 23;
-    int *myPointer = &variable1;
+    cout << "Array address: " << array << endl;
+    cout << "Array address2: " << &array[0] << endl;
 
-    cout << "Var 1 value: " << variable1 << endl;
-    cout << "Var 1 address: " << myPointer << endl;
-    cout << "Var 1 through pointer: " << *myPointer << endl;
-    cout << "Var 1 location of the pointer: " << &myPointer << endl; 
+    cout << "Array third element: " << array[2] << endl;
+    cout << "Array third element2: " << *(array + 2) << endl;
 
-    // "*" star operator, allows pointer declaration and access to the value of the variable pointed by the pointer.
-    // "&" ampersand operator, allows address extraction of a variable.
+    int luckyNumbers[5];
 
-    printNumber(myPointer);
-    printNumber(&variable1);
+    for(int i = 0; i < 5; i++)
+    {
+        cout << "Enter number: ";
+        cin >> luckyNumbers[i];
+    }
 
+    for(int i = 0; i < 5; i++)
+    {
+        cout << *(luckyNumbers + i) << " ";
+    }
+
+    cout << " " << endl;
     return 0;
-}
-
-void printNumber(int *printNumber)
-{
-    // The parameter of this function is a pointer, therefore we have to parse it a pointer.
-    // But we also can also parse the address of a variable using the "&" operator.
-    cout << "Print number with pointer parameter called: " << *printNumber << endl;
 }
